09bcaca73befa6156aace326162b04a4a5749069
avanilkumar <avanilkumar@gmail.com>
2016-05-14 14:41:57 +0530

    Add custom toolbar

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.android.courtcounter;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +20,15 @@ public class MainActivity extends AppCompatActivity {
     private final String teamB = "Team B";
     private int teamAScore;
     private int teamBScore;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
-    public void updateScores(){
-        ( (TextView) findViewById(R.id.teamA).findViewById(R.id.score)).setText(""+teamAScore);
-        ( (TextView) findViewById(R.id.teamB).findViewById(R.id.score)).setText(""+teamBScore);
+    public void updateScores() {
+        ((TextView) findViewById(R.id.teamA).findViewById(R.id.score)).setText("" + teamAScore);
+        ((TextView) findViewById(R.id.teamB).findViewById(R.id.score)).setText("" + teamBScore);
     }
 
     @Override
@@ -25,53 +36,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setSupportActionBar((Toolbar) findViewById(R.id.app_bar));
+        //setActionBar((Toolbar)findViewById(R.id.app_bar));
+         ((Toolbar) findViewById(R.id.app_bar)).showOverflowMenu();
+
         ((TextView) findViewById(R.id.teamA).findViewById(R.id.team)).setText(teamA);
         ((TextView) findViewById(R.id.teamB).findViewById(R.id.team)).setText(teamB);
 
-        if(savedInstanceState==null){
+        if (savedInstanceState == null) {
             teamAScore = teamBScore = 0;
-        }else{
+        } else {
             teamAScore = savedInstanceState.getInt(teamA);
             teamBScore = savedInstanceState.getInt(teamB);
         }
-
         updateScores();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt(teamA,teamAScore);
-        outState.putInt(teamB,teamBScore);
+        outState.putInt(teamA, teamAScore);
+        outState.putInt(teamB, teamBScore);
     }
 
     public void freeThrow(View view) {
 
-        if(((ViewGroup)view.getParent()).getId() == R.id.teamA){
+        if (((ViewGroup) view.getParent()).getId() == R.id.teamA) {
             teamAScore++;
-        }else{
+        } else {
             teamBScore++;
         }
         updateScores();
     }
 
     public void plusTwo(View view) {
-        if(((ViewGroup)view.getParent()).getId() == R.id.teamA){
-            teamAScore+=2;
-        }else{
-            teamBScore+=2;
+        if (((ViewGroup) view.getParent()).getId() == R.id.teamA) {
+            teamAScore += 2;
+        } else {
+            teamBScore += 2;
         }
         updateScores();
 
     }
 
     public void plusThree(View view) {
-        if(((ViewGroup)view.getParent()).getId() == R.id.teamA){
-            teamAScore+=3;
-        }else{
-            teamBScore+=3;
+        if (((ViewGroup) view.getParent()).getId() == R.id.teamA) {
+            teamAScore += 3;
+        } else {
+            teamBScore += 3;
         }
         updateScores();
 
@@ -80,6 +97,46 @@ public class MainActivity extends AppCompatActivity {
     public void Reset(View view) {
         teamAScore = teamBScore = 0;
         updateScores();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.android.courtcounter/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.android.courtcounter/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
 
